323c0db7c5e951b0948a7f045c38078332c5d7d3
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 21:50:27 +0530

    Add files via upload

---
+++ Pass_or_Fail.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Pass_or_Fail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }   
+}
+
+public class Pass_or_Fail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }   
+}
+
+public class Pass_or_Fail{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int marks=sc.nextInt();
+        if(marks>=40){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }   
+}
