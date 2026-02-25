7f606e1c01850b8191cdcf725be85c769a1fa1ef
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 21:59:04 +0530

    Add files via upload

---
+++ Greatest_of_two.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Greatest_of_two{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int d=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int e=sc.nextInt();
+        if(d>e){
+            System.out.println("Greatest: "+d);
+        }else{
+            System.out.println("Greatest: "+e);
+        }
+        sc.close();
+    }
+}
+
+public class Greatest_of_two{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int d=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int e=sc.nextInt();
+        if(d>e){
+            System.out.println("Greatest: "+d);
+        }else{
+            System.out.println("Greatest: "+e);
+        }
+        sc.close();
+    }
+}
+
+public class Greatest_of_two{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int d=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int e=sc.nextInt();
+        if(d>e){
+            System.out.println("Greatest: "+d);
+        }else{
+            System.out.println("Greatest: "+e);
+        }
+        sc.close();
+    }
+}
