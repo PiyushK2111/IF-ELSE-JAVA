b28dd729e6e0018e4e450e07b49dc818d8c5a279
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 18:31:18 +0530

    Add files via upload

---
+++ Even_Odd_Check.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Even_Odd_Check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num =sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+        sc.close();
+    }
+}
+
+public class Even_Odd_Check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num =sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+        sc.close();
+    }
+}
+
+public class Even_Odd_Check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num =sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+        sc.close();
+    }
+}   
