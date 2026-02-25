bac69b7103723254372f55e76a4db56ca3af6a6b
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 21:40:04 +0530

    Add files via upload

---
+++ Positive_Negative_Zero.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Positive_Negative_Zero{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n==0){
+            System.out.println("Zero");
+        }else{
+            System.out.println("Negative");
+        }
+        sc.close();
+    }
+}
+
+public class Positive_Negative_Zero{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n==0){
+            System.out.println("Zero");
+        }else{
+            System.out.println("Negative");
+        }
+        sc.close();
+    }
+}
+
+public class Positive_Negative_Zero{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }else if(n==0){
+            System.out.println("Zero");
+        }else{
+            System.out.println("Negative");
+        }
+        sc.close();
+    }
+}
