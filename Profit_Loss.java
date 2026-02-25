10b384713ec40c101b44600fb004b2392b6976a5
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 22:23:26 +0530

    Add files via upload

---
+++ Profit_Loss.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Profit_Loss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter cost price: ");
+        int cost=sc.nextInt();
+        System.out.print("Enter selling price: ");
+        int sell=sc.nextInt();
+        if(sell>cost){
+            System.out.println("Profit");
+        }else if(sell<cost){
+            System.out.println("Loss");
+        }else{
+            System.out.println("No Profit No Loss");
+        }
+        sc.close();
+    }
+}
+
+public class Profit_Loss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter cost price: ");
+        int cost=sc.nextInt();
+        System.out.print("Enter selling price: ");
+        int sell=sc.nextInt();
+        if(sell>cost){
+            System.out.println("Profit");
+        }else if(sell<cost){
+            System.out.println("Loss");
+        }else{
+            System.out.println("No Profit No Loss");
+        }
+        sc.close();
+    }
+}
+
+public class Profit_Loss{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter cost price: ");
+        int cost=sc.nextInt();
+        System.out.print("Enter selling price: ");
+        int sell=sc.nextInt();
+        if(sell>cost){
+            System.out.println("Profit");
+        }else if(sell<cost){
+            System.out.println("Loss");
+        }else{
+            System.out.println("No Profit No Loss");
+        }
+        sc.close();
+    }
+}
