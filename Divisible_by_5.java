57fd45e4fd83e03ff58f9fda2296400f333c1ce4
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 22:15:25 +0530

    Add files via upload

---
+++ Divisible_by_5.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Divisible_by_5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        if(num%5==0){
+            System.out.println("Divisible");
+        }else{
+            System.out.println("Not divisible");
+        }
+        sc.close();
+    }
+}
+
+public class Divisible_by_5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        if(num%5==0){
+            System.out.println("Divisible");
+        }else{
+            System.out.println("Not divisible");
+        }
+        sc.close();
+    }
+}
+
+public class Divisible_by_5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        if(num%5==0){
+            System.out.println("Divisible");
+        }else{
+            System.out.println("Not divisible");
+        }
+        sc.close();
+    }
+}
