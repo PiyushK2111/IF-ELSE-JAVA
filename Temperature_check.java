069b0023a72da34e7c06313d5f6afc3c3b7f05e9
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 22:18:48 +0530

    Add files via upload

---
+++ Temperature_check.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Temperature_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter temperature: ");
+        int temp=sc.nextInt();
+        if(temp>25){
+            System.out.println("Hot");
+        }else{
+            System.out.println("Cold");
+        }
+        sc.close();
+    }
+}
+
+public class Temperature_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter temperature: ");
+        int temp=sc.nextInt();
+        if(temp>25){
+            System.out.println("Hot");
+        }else{
+            System.out.println("Cold");
+        }
+        sc.close();
+    }
+}
+
+public class Temperature_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter temperature: ");
+        int temp=sc.nextInt();
+        if(temp>25){
+            System.out.println("Hot");
+        }else{
+            System.out.println("Cold");
+        }
+        sc.close();
+    }
+}
