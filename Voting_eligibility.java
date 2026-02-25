9768bf7d97b99e1ea7291bf202cbb27e9749ae9c
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-25 22:11:31 +0530

    Add files via upload

---
+++ Voting_eligibility.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Voting_eligibility{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
+
+public class Voting_eligibility{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
+
+public class Voting_eligibility{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
