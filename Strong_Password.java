355db6289dba0cf9621542e8f10a2317dae7206b
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 23:59:25 +0530

    Add files via upload

---
+++ Strong_Password.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Strong_Password{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.length()>=8 && pass.matches(".*[A-Z].*") && pass.matches(".*[a-z].") && pass.matches(".*[0-9].*")){
+            System.out.println("Strong Password");
+        }else{
+            System.out.println("Weak Password");
+        }
+        sc.close();
+    }
+}
+
+public class Strong_Password{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.length()>=8 && pass.matches(".*[A-Z].*") && pass.matches(".*[a-z].") && pass.matches(".*[0-9].*")){
+            System.out.println("Strong Password");
+        }else{
+            System.out.println("Weak Password");
+        }
+        sc.close();
+    }
+}
+
+public class Strong_Password{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.length()>=8 && pass.matches(".*[A-Z].*") && pass.matches(".*[a-z].") && pass.matches(".*[0-9].*")){
+            System.out.println("Strong Password");
+        }else{
+            System.out.println("Weak Password");
+        }
+        sc.close();
+    }
+}
