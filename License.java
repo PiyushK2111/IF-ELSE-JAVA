4eabcd5bb1666907633fcfa3040ef794ae1d8729
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 23:21:31 +0530

    Add files via upload

---
+++ License.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class License{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Can apply for license");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
+
+public class License{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Can apply for license");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
+
+public class License{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Can apply for license");
+        }else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
