3253f9846ce90d4ce7b9306050e910319a8336c9
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 18:17:45 +0530

    Add files via upload

---
+++ Simple_Interest.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class Simple_Interest{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter principle: ");
+        int p=sc.nextInt();
+        System.out.print("Enter rate: ");
+        int r=sc.nextInt();
+        System.out.print("Enter time: ");
+        int t=sc.nextInt();
+        int si=(p*r*t)/100;
+        if(si>100){
+            System.out.println("High interest");
+        }else{
+            System.out.println("Low interest");
+        }
+        sc.close();
+    }
+}
+
+public class Simple_Interest{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter principle: ");
+        int p=sc.nextInt();
+        System.out.print("Enter rate: ");
+        int r=sc.nextInt();
+        System.out.print("Enter time: ");
+        int t=sc.nextInt();
+        int si=(p*r*t)/100;
+        if(si>100){
+            System.out.println("High interest");
+        }else{
+            System.out.println("Low interest");
+        }
+        sc.close();
+    }
+}
+
+public class Simple_Interest{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter principle: ");
+        int p=sc.nextInt();
+        System.out.print("Enter rate: ");
+        int r=sc.nextInt();
+        System.out.print("Enter time: ");
+        int t=sc.nextInt();
+        int si=(p*r*t)/100;
+        if(si>100){
+            System.out.println("High interest");
+        }else{
+            System.out.println("Low interest");
+        }
+        sc.close();
+    }
+}
