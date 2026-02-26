d6a6acdbff017687b6a66ed9ed6c4aedb17fbcd8
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 18:21:36 +0530

    Add files via upload

---
+++ Password_validation.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Password_validation{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.equals("Bewakoof")){
+            System.out.println("Access Granted");
+        }else{
+            System.out.println("Access Denied");
+        }
+        sc.close();
+    }
+}
+
+public class Password_validation{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.equals("Bewakoof")){
+            System.out.println("Access Granted");
+        }else{
+            System.out.println("Access Denied");
+        }
+        sc.close();
+    }
+}
+
+public class Password_validation{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass=sc.nextLine();
+        if(pass.equals("Bewakoof")){
+            System.out.println("Access Granted");
+        }else{
+            System.out.println("Access Denied");
+        }
+        sc.close();
+    }
+}
