8326a5dfc8760584b068a6b5224e1ea82bb1ae32
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 23:38:23 +0530

    Add files via upload

---
+++ ATM_Withdrawal.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class ATM_Withdrawal{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter balance: ");
+        double balance= sc.nextDouble();
+        System.out.print("Enter withdrawal amount: ");
+        double amount=sc.nextDouble();
+        if(amount<=balance){
+            System.out.println("Transaction Successful");
+        }else{
+            System.out.println("Insufficient Balance");
+        }
+        sc.close();
+    }
+}
+
+public class ATM_Withdrawal{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter balance: ");
+        double balance= sc.nextDouble();
+        System.out.print("Enter withdrawal amount: ");
+        double amount=sc.nextDouble();
+        if(amount<=balance){
+            System.out.println("Transaction Successful");
+        }else{
+            System.out.println("Insufficient Balance");
+        }
+        sc.close();
+    }
+}
+
+public class ATM_Withdrawal{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter balance: ");
+        double balance= sc.nextDouble();
+        System.out.print("Enter withdrawal amount: ");
+        double amount=sc.nextDouble();
+        if(amount<=balance){
+            System.out.println("Transaction Successful");
+        }else{
+            System.out.println("Insufficient Balance");
+        }
+        sc.close();
+    }
+}
