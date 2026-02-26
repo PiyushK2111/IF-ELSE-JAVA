8b032bbb50cd6953b25b454ac1ae8e7eef8e0562
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 23:40:53 +0530

    Add files via upload

---
+++ Range_check.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Range_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n>=1 && n<=100){
+            System.out.println("Within Range");
+        }else{
+            System.out.println("Out of Range");
+        }
+        sc.close();
+    }
+}
+
+public class Range_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n>=1 && n<=100){
+            System.out.println("Within Range");
+        }else{
+            System.out.println("Out of Range");
+        }
+        sc.close();
+    }
+}
+
+public class Range_check{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n>=1 && n<=100){
+            System.out.println("Within Range");
+        }else{
+            System.out.println("Out of Range");
+        }
+        sc.close();
+    }
+}
