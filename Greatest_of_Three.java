fae045a4c876c646536a747122c38858b4716e1b
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 18:01:58 +0530

    Add files via upload

---
+++ Greatest_of_Three.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Greatest_of_Three{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c=sc.nextInt(); 
+        if(a>=b && a>=c){
+            System.out.println("Greatest: "+a);
+        }else if(b>=c){
+            System.out.println("Greatest: "+b);
+        }else{
+            System.out.println("Greatest: "+c);
+        }
+        sc.close();
+    }
+   
+}
+
+public class Greatest_of_Three{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c=sc.nextInt(); 
+        if(a>=b && a>=c){
+            System.out.println("Greatest: "+a);
+        }else if(b>=c){
+            System.out.println("Greatest: "+b);
+        }else{
+            System.out.println("Greatest: "+c);
+        }
+        sc.close();
+    }
+   
+}
+
+public class Greatest_of_Three{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c=sc.nextInt(); 
+        if(a>=b && a>=c){
+            System.out.println("Greatest: "+a);
+        }else if(b>=c){
+            System.out.println("Greatest: "+b);
+        }else{
+            System.out.println("Greatest: "+c);
+        }
+        sc.close();
+    }
+   
+}
