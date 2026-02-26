aa92f4d25dcb056be2e505b27b13866d525d07d3
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 18:07:44 +0530

    Add files via upload

---
+++ Grade_calculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Grade_calculator{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int a=sc.nextInt();
+        if(a>=90){
+            System.out.println("Grade A");
+        }else if(a>=75){
+            System.out.println("Grade B");
+        }else if(a>=50){
+            System.out.println("Grade C");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }
+}
+
+public class Grade_calculator{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int a=sc.nextInt();
+        if(a>=90){
+            System.out.println("Grade A");
+        }else if(a>=75){
+            System.out.println("Grade B");
+        }else if(a>=50){
+            System.out.println("Grade C");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }
+}
+
+public class Grade_calculator{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int a=sc.nextInt();
+        if(a>=90){
+            System.out.println("Grade A");
+        }else if(a>=75){
+            System.out.println("Grade B");
+        }else if(a>=50){
+            System.out.println("Grade C");
+        }else{
+            System.out.println("Fail");
+        }
+        sc.close();
+    }
+}
