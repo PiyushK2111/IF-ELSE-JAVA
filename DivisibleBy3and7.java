85b2fa248068e26518c262e8fdd0b5d8b32bc87f
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-26 18:44:27 +0530

    Add files via upload

---
+++ DivisibleBy3and7.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class DivisibleBy3and7{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n%3==0 && n%7==0){
+            System.out.println("Divisible by both 3 and 7");
+        }else{
+            System.out.println("Not divisible by both");
+        }
+        sc.close();  
+    }
+}
+
+public class DivisibleBy3and7{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n%3==0 && n%7==0){
+            System.out.println("Divisible by both 3 and 7");
+        }else{
+            System.out.println("Not divisible by both");
+        }
+        sc.close();  
+    }
+}
+
+public class DivisibleBy3and7{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        if(n%3==0 && n%7==0){
+            System.out.println("Divisible by both 3 and 7");
+        }else{
+            System.out.println("Not divisible by both");
+        }
+        sc.close();  
+    }
+}
